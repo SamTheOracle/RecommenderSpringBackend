3368333960c2c497fc364001eae90ea0d8b47c98
SamTheOracle <gzano93@gmail.com>
2019-01-15 23:37:00 +0100

    physical activities server with controller added

---
+++ src/main/java/com/app/recommender/physicalactivities/PhysicalActivitiesController.java
@@ -0,0 +1,22 @@
+package com.app.recommender.physicalactivities;
+
+import com.app.recommender.Model.Cycling;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class PhysicalActivitiesController {
+
+    @GetMapping(value = "/{physicalActivityName}")
+    public ResponseEntity getPhysicalActivity(@PathVariable(value = "physicalActivityName") String physicalActivityName) {
+        return ResponseEntity.status(200).body(new Cycling());
+    }
+    @GetMapping(value = "/test")
+    public int test(){
+        return 2;
+    }
+}
--- src/main/java/com/app/recommender/physicalactivities/PhysicalActivitiesServer.java
+++ src/main/java/com/app/recommender/physicalactivities/PhysicalActivitiesServer.java
@@ -1,18 +1,16 @@
 package com.app.recommender.physicalactivities;
 
-import com.app.recommender.user.RegistrationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-
 public class PhysicalActivitiesServer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.setProperty("spring.config.name", "physicalactivities-server");
 
-        SpringApplication.run(RegistrationServer.class, args);
+        SpringApplication.run(PhysicalActivitiesServer.class, args);
     }
 }
