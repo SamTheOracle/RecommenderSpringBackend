f87157d099bde0822eaae9ec60a80c484dc449ec
SamTheOracle <gzano93@gmail.com>
2019-02-14 21:52:09 +0100

    minor

--- src/main/java/com/app/recommender/gateway/GateWayController.java
+++ src/main/java/com/app/recommender/gateway/GateWayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @RestController
 public class GateWayController {
@@ -84,9 +85,9 @@ public class GateWayController {
 
     }
     @GetMapping(value = "/services")
-    public List<ServiceInstance> getServices(){
+    public List<String> getServices(){
 
-        return discoveryClient.getInstances("registrations-microservice");
+        return discoveryClient.getInstances("registrations-microservice").stream().map(serviceInstance -> serviceInstance.getUri()+"\n "+serviceInstance.getHost()).collect(Collectors.toList());
     }
 
 
