cc1d13b92c967c9b30b329af45c5ef7d7ae6d7f1
SamTheOracle <gzano93@gmail.com>
2019-05-22 13:17:09 +0200

    example of discovery client fetching some data from foodrec and fixed ports

--- src/main/java/com/app/recommender/greetings/GreetingsController.java
+++ src/main/java/com/app/recommender/greetings/GreetingsController.java
@@ -1,18 +1,51 @@
 package com.app.recommender.greetings;
 
-import org.springframework.http.MediaType;
+import com.app.recommender.Model.FoodRdf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 public class GreetingsController {
 
-    @GetMapping(value="/{userName}", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity showUserName(@PathVariable(value = "userName") String userName){
-        return ResponseEntity.ok("Hi "+userName +" good to have you here!");
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/{userName}")
+    public ResponseEntity getStuffWithDiscovery(@PathVariable(value = "userName") String userName) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("food-microservice");
+        Optional<ServiceInstance> instanceOptional = instances.stream().findAny();
+        if (instanceOptional.isPresent()) {
+            ServiceInstance instance = instanceOptional.get();
+            RestTemplate restTemplate = new RestTemplate();
+            int port = instance.getPort();
+            String host = instance.getHost();
+            String uri = "http://" + host + ":" + port + "/Dieta Aprile Ufficiale/suggestions/FruitsAndVegetables?userId=21bd08ee-8ee7-f1bd-d379-9800771f6f81";
+            ResponseEntity<FoodRdf[]> response = restTemplate.getForEntity(uri, FoodRdf[].class);
+            return ResponseEntity.ok(Objects.requireNonNull(response.getBody()));
+        }
+
+        return ResponseEntity.ok("Hi " + userName + " good to have you here!");
     }
+
+    @GetMapping(value = "/instances")
+    public ResponseEntity showUserName(@PathVariable(value = "userName") String userName) {
+
+
+        return ResponseEntity.ok("Hi " + userName + " good to have you here!");
+    }
+
 }
--- src/main/java/com/app/recommender/greetings/GreetingsServer.java
+++ src/main/java/com/app/recommender/greetings/GreetingsServer.java
@@ -1,9 +1,10 @@
 package com.app.recommender.greetings;
 
-import com.app.recommender.records.RecordServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,6 +12,11 @@ public class GreetingsServer {
     public static void main(String[] args) {
         System.setProperty("spring.config.name", "greeting-server");
         SpringApplication.run(GreetingsServer.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
